498b2844ff290731ccb6f392f4bfe61d2fd4337c
NitroRonak <ronaks0988@gmail.com>
2023-11-08 23:20:03 +0530

    Modified first section of home page

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,12 +1,17 @@
 // import Header from '../Components/Header'
 import Navbar from "../Components/Navbar";
-
+import Pain1 from "../assets/images/pain1.png"
 const Home = () => {
   return (
     <>
       <main>
-        <section className="h-screen bg-black ">
+        <section className="h-screen pain">
           <Navbar />
+          <div className="w-2/3 h-4/5 flex justify-center">
+            <h1 className="text-white font-bold text-6xl self-center">
+              Welcome To <span className="text-orange-400">AniHome</span>
+            </h1>
+          </div>
         </section>
       </main>
     </>
