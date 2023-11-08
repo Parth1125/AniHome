3928c7a4beb2738ac1bc59816b3017ade1e3c005
Parth1125 <pgera326@gmail.com>
2023-11-08 21:49:05 +0530

    inital commit

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,23 +1,22 @@
 // import Header from "./Header";
 
-
 import Searchbar from "./Searchbar";
 
 const Navbar = () => {
   return (
     <>
       <nav className="w-full h-16  flex text-black justify-between px-4 items-center md:px-4 bg-transparent ">
-        <div className="text-xl md:text-2xl text-indigo-700 font-bold">AniHome</div>
-        <Searchbar/>
+        <div className="text-xl md:text-2xl text-white font-bold">AniHome</div>
+        <Searchbar />
         <div className="hidden md:block px-2 py-2 bg-indigo-700 text-white font-bold cursor-pointer">
           Login/Signup
         </div>
         <div className="md:hidden">
-          <a className="text-4xl" href="#">&#8801;</a>
+          <a className="text-4xl" href="#">
+            &#8801;
+          </a>
         </div>
       </nav>
-      
-     
     </>
   );
 };
