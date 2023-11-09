87172faae69f73da0bde6e1cc204e3e6222d1579
Parth1125 <pgera326@gmail.com>
2023-11-09 23:06:10 +0530

    solved errors

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,17 +1,22 @@
 // import Header from "./Header";
 import { AiOutlineSearch } from "react-icons/ai";
+import logo from "../assets/images/anihome.png";
 const Navbar = () => {
   return (
     <>
       <nav className="w-full h-16  flex text-black justify-between px-4 items-center md:px-4 bg-transparent z-[5]">
-        <div className="text-xl md:text-2xl text-indigo-700 font-bold">
-          AniHome
+        <div className="bg-black rounded-lg">
+          <img
+            src={logo}
+            alt=""
+            className="lg:w-36 lg:h-14 md:w-28 md:h-10 sm:w-28 h-7 w-24"
+          />
         </div>
 
-        <div className="flex justify-center items-center p-2 ">
+        <div className="flex justify-center items-center sm:p-2 p-1 bg-white rounded-lg">
           <input
             type="text"
-            className="bg-transparent rounded-md text-white focus:outline-none"
+            className="md:w-80 sm:w-40 w-28 bg-transparent rounded-md text-black focus:outline-none"
             placeholder="Search..."
           />
           <AiOutlineSearch className="cursor-pointer text-black" />
